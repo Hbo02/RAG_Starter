b5b9f1e89359e3457362d69e2bc33ceeda0b800e
Hbo02 <hbohas007@gmail.com>
2025-06-18 19:53:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-## retriever/faiss_retriever.py
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
-
-def load_retriever():
-    embeddings = OpenAIEmbeddings()
-    db = FAISS.load_local("faiss_index", embeddings)
-    return db
-
----
-
 ## llm/rag_chain.py
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
