4672ef23d24dcd541fa2b865eaf371344c81683e
Hbo02 <hbohas007@gmail.com>
2025-06-18 19:54:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-## llm/rag_chain.py
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from retriever.faiss_retriever import load_retriever
-
-retriever = load_retriever()
-llm = ChatOpenAI(temperature=0)
-rag_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
-
-def ask_rag(query):
-    return rag_chain.run(query)
-
----
-
 ## guardrails/safety_rails.gr.xml
 <guardrails>
   <output>
