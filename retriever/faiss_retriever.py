0b03e3e058f4988ed29cae7252e8f1b589d9ce28
Hbo02 <hbohas007@gmail.com>
2025-06-19 21:53:46 +0200

    Update faiss_retriever.py

--- retriever/faiss_retriever.py
+++ retriever/faiss_retriever.py
@@ -1,12 +1,15 @@
 ## retriever/faiss_retriever.py
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
-
+from ingest.ingest_docs import ingest
 import utils.config 
 from utils.config import get_env_var
-openai_key = get_env_var("OPENAI_API_KEY")
+openai_api_key = get_env_var("OPENAI_API_KEY")
 
 def load_retriever():
-    embeddings = OpenAIEmbeddings(openai_api_key = openai_key)
+    if not os.path.exists("faiss_index"):
+        print("FAISS index not found. Running ingestion...")
+        ingest()
+    embeddings = OpenAIEmbeddings(openai_api_key = openai_api_key)
     db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     return db
