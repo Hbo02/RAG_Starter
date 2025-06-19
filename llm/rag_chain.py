9c3747375f2975c2d77701f0e57ad60d7e7914bf
Hbo02 <hbohas007@gmail.com>
2025-06-19 13:14:41 +0200

    Update rag_chain.py

--- llm/rag_chain.py
+++ llm/rag_chain.py
@@ -2,9 +2,13 @@
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from retriever.faiss_retriever import load_retriever
+import utils.config 
+from utils.config import get_env_var
+
+openai_api_key=get_env_var("OPENAI_API_KEY")
 
 retriever = load_retriever()
-llm = ChatOpenAI(temperature=0)
+llm = ChatOpenAI(temperature=0, openai_api_key = openai_api_key)
 rag_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 
 def ask_rag(query):
