04ec7b2be4e1cf0287e8e91088588b0e803294a2
Hbo02 <hbohas007@gmail.com>
2025-06-19 13:15:53 +0200

    Update faiss_retriever.py

--- retriever/faiss_retriever.py
+++ retriever/faiss_retriever.py
@@ -2,7 +2,11 @@
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 
+import utils.config 
+from utils.config import get_env_var
+openai_key = get_env_var("OPENAI_API_KEY")
+
 def load_retriever():
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(openai_key = openai_key)
     db = FAISS.load_local("faiss_index", embeddings)
     return db
